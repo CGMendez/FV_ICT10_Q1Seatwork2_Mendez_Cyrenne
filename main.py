ff77ae56d5be0e918426860e4aff10f4a01e7f27
CGMendez <cyrennerobloxgamez@gmail.com>
2025-09-04 11:30:54 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyscript import display
+
+restaurant_name = "RRC Raunts"
+display(restaurant_name)
+# the restaurant name variable alongside the data is called a string. The name of my restaurant comes from my parents' and my initials.
+
+owner_name = "Owner: Cyrenne Grace P. Mendez"
+display(owner_name)
+# the owner name variable alongside the data is called a string. 
+
+year_established = "Since September 3, 2025"
+display(year_established)
+# the year established variable alongside the data is called a string.
