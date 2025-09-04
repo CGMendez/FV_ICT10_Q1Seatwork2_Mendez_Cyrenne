803a4cc31af7d1c81372b2b012ed0f0799bcdf1a
CGMendez <cyrennerobloxgamez@gmail.com>
2025-09-04 21:08:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyscript import display
 
 restaurant_name = "RRC Raunts"
 display(restaurant_name, target="header")
-# the restaurant name variable alongside the data is called a string. The name of my restaurant comes from my parents' and my initials.
+# the restaurant name variable alongside the data is called a string. 
 
 owner_name = "Owner: Cyrenne Grace P. Mendez"
 display(owner_name, target="header")
@@ -12,18 +12,24 @@ year_established = 2025
 display(f"Year Established: {year_established}", target="header")
 # the year established variable alongside the data is called an integer.
 
-#popular_item_price = 150
-# the popular item price variable alongside the data is called an integer.
-#display(f"Popular Item Price: ₱{popular_item_price}")
+popular_item_price = (150, 215)
+# the popular item price variable alongside the data is called an tuple.
 
-#has_delivery = True
+has_delivery = True
 # the has delivery variable alongside the data is called a boolean.
-#display(f"Has Delivery: {has_delivery}")
 
-#product_names = ['Adobo', 'Bisaya Utan', 'Lechon Kawali', 'Pancit-bihon', 'Kare-Kare']
+product_names = ['Adobo', 'Bisaya Utan', 'Lechon Kawali', 'Pancit-bihon', 'Kare-Kare']
 # the product names variable alongside the data is called a list. 
-#display("Product Names:")
 
 business_hours = "Opening Hours: 8:00 AM - 12:00 AM" 
 display(business_hours, target="footer")
 # the business hours variable alongside the data is called a string.
+
+menu_prices = {'Adobo': 150, 'Bisaya Utan': 200, 'Lechon Kawali': 215, 'Pancit-bihon': 130, 'Kare-Kare': 215}
+# the menu prices variable alongside the data is called a dictionary.
+
+common_allergens = {'Soy, Wheat, Peanuts'}
+# the common allergens variable alongside the data is called a set.
+
+tax_rate = 0.12
+# the tax rate variable alongside the data is called a float.
