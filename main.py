3b4619bcb3eab2f2e53fd747deb722c3f313c1be
CGMendez <cyrennerobloxgamez@gmail.com>
2025-09-04 17:08:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from pyscript import display
 
 restaurant_name = "RRC Raunts"
-display(restaurant_name)
+display(restaurant_name, target="header")
 # the restaurant name variable alongside the data is called a string. The name of my restaurant comes from my parents' and my initials.
 
 owner_name = "Owner: Cyrenne Grace P. Mendez"
-display(owner_name)
+display(owner_name, target="header")
 # the owner name variable alongside the data is called a string. 
 
-year_established = "Since September 3, 2025"
-display(year_established)
-# the year established variable alongside the data is called a string.
+year_established = 2025
+display(f"Year Established: {year_established}", target="header")
+# the year established variable alongside the data is called an integer.
+
+#popular_item_price = 150
+# the popular item price variable alongside the data is called an integer.
+#display(f"Popular Item Price: ₱{popular_item_price}")
+
+#has_delivery = True
+# the has delivery variable alongside the data is called a boolean.
+#display(f"Has Delivery: {has_delivery}")
+
+#product_names = ['Adobo', 'Bisaya Utan', 'Lechon Kawali', 'Pancit-bihon', 'Kare-Kare']
+# the product names variable alongside the data is called a list. 
+#display("Product Names:")
+
+business_hours = "Open from 8:00 AM to 12:00 AM"
+display(business_hours, target="footer")
+# the business hours variable alongside the data is called a string.
